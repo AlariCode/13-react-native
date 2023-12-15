1f1a54a9444ad8398c4ec8f5e603593d949dab1c
AlariCode <antonlarichev@gmail.com>
2023-12-15 16:07:45 +0300

    Основы flex

--- App.tsx
+++ App.tsx
@@ -4,23 +4,20 @@ import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 export default function App() {
 	return (
 		<View style={styles.container}>
-			<View>
-				<Text>Привет!</Text>
+			<View style={styles.top}>
+				<Text style={styles.textStyle}>Моё первое приложение</Text>
+				<Button title="Я кнопка" />
 			</View>
-			<Text style={styles.textStyle}>Моё первое приложение</Text>
-			<StatusBar style="auto" />
-			<TextInput value='привет!' />
-			<Button title="Я кнопка" />
 		</View>
 	);
 }
 
 const styles = StyleSheet.create({
 	container: {
-		flex: 1,
-		backgroundColor: '#fff',
-		alignItems: 'center',
-		justifyContent: 'center',
+		marginTop: 70
+	},
+	top: {
+		flexDirection: 'row'
 	},
 	textStyle: { color: 'blue', fontSize: 24, borderWidth: 1, borderColor: 'blue' }
 });
