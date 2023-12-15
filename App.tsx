f56d3a9e67ff16833fbdd3268257fd1cd0c91c8a
AlariCode <antonlarichev@gmail.com>
2023-12-15 11:22:49 +0300

    Использование компонент

--- App.tsx
+++ App.tsx
@@ -1,20 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<View style={styles.container}>
+			<View>
+				<Text>Привет!</Text>
+			</View>
+			<Text>Моё первое приложение</Text>
+			<StatusBar style="auto" />
+			<TextInput value='привет!' />
+			<Button title="Я кнопка" />
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
 });
