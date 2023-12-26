eb54733553fe90be563144226b6c3b3ed5e4c431
AlariCode <antonlarichev@gmail.com>
2023-12-26 16:56:33 +0300

    Как работает анимация

--- shared/Button/Button.tsx
+++ shared/Button/Button.tsx
@@ -1,18 +1,22 @@
 import { Animated, Pressable, PressableProps, StyleSheet, Text, View } from 'react-native';
 import { Colors, Fonts, Radius } from '../tokens';
+import { useEffect } from 'react';
 
 export function Button({ text, ...props }: PressableProps & { text: string }) {
 	const animatedValue = new Animated.ValueXY({
 		x: 0,
 		y: 0
 	});
-	Animated.spring(animatedValue, {
+
+	Animated.timing(animatedValue, {
 		toValue: {
 			x: 100,
 			y: 100
 		},
+		duration: 2000,
 		useNativeDriver: true
 	}).start();
+
 	return (
 		<Pressable {...props}>
 			<Animated.View style={{
