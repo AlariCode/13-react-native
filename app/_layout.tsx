b88b658ddd58b4f6516d03d3b68be0c6193e73c5
AlariCode <antonlarichev@gmail.com>
2024-01-11 18:39:26 +0300

    Stack

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,24 @@
 import { Stack } from 'expo-router';
+import { Colors } from '../shared/tokens';
 
 export default function RootRayout() {
-	return <Stack />;
+	return (
+		<Stack
+			screenOptions={{
+				statusBarColor: Colors.black,
+				contentStyle: {
+					backgroundColor: Colors.black,
+				},
+			}}
+		>
+			<Stack.Screen name="index" />
+			<Stack.Screen
+				name="restore"
+				options={{
+					presentation: 'modal',
+					headerShown: false,
+				}}
+			/>
+		</Stack>
+	);
 }
