99fde3745844323449abff7b4d02f8decd3c44e1
AlariCode <antonlarichev@gmail.com>
2024-01-18 12:30:06 +0300

    AsyncStorage

--- app/(app)/index.tsx
+++ app/(app)/index.tsx
@@ -1,9 +1,21 @@
 import { useAtom } from 'jotai';
 import { View, Text } from 'react-native';
 import { profileAtom } from '../../entities/user/model/user.state';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useEffect } from 'react';
 
 export default function MyCourses() {
 	const [profile] = useAtom(profileAtom);
+
+	useEffect(() => {
+		AsyncStorage.setItem('demo', 'test').then(async () => {
+			console.log(await AsyncStorage.getAllKeys());
+			console.log(await AsyncStorage.getItem('demo'));
+			console.log(await AsyncStorage.removeItem('demo'));
+			console.log(await AsyncStorage.getItem('demo'));
+		});
+	}, []);
+
 	return (
 		<View>
 			<Text>{profile.profile?.name}</Text>
