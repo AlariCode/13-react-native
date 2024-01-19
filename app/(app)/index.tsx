74ab0cd0863000710d9c9fb8f30cc94e5e2ff6f2
AlariCode <antonlarichev@gmail.com>
2024-01-19 12:14:07 +0300

    Запросы на сервер

--- app/(app)/index.tsx
+++ app/(app)/index.tsx
@@ -1,9 +1,26 @@
 import { useAtom } from 'jotai';
 import { View, Text } from 'react-native';
 import { profileAtom } from '../../entities/user/model/user.state';
+import axios from 'axios';
+import { API } from '../../entities/auth/api/api';
+import { useEffect } from 'react';
+import { IAuthResponse } from '../../entities/auth/model/auth.intefaces';
 
 export default function MyCourses() {
 	const [profile] = useAtom(profileAtom);
+
+	const login = async () => {
+		const { data } = await axios.post<IAuthResponse>(API.login, {
+			email: 'vasia@pupkin.ru',
+			password: '123456789',
+		});
+		console.log(data);
+	};
+
+	useEffect(() => {
+		login();
+	}, []);
+
 	return (
 		<View>
 			<Text>{profile.profile?.name}</Text>
---
+++ entities/auth/api/api.ts
@@ -0,0 +1,5 @@
+export const PREFIX = 'https://purpleschool.ru/api-v2';
+
+export const API = {
+	login: `${PREFIX}/auth/login`,
+};
