962bcf613d2a35ad2979e8ca6e11ab271ac74cce
AlariCode <antonlarichev@gmail.com>
2024-01-23 15:43:44 +0300

    Redirect компонент

--- app/(app)/_layout.tsx
+++ app/(app)/_layout.tsx
@@ -1,6 +1,13 @@
-import { Stack } from 'expo-router';
+import { Redirect, Stack } from 'expo-router';
+import { useAtomValue } from 'jotai';
+import { authAtom } from '../../entities/auth/model/auth.state';
 
 export default function AppRayout() {
+	const { access_token } = useAtomValue(authAtom);
+	if (!access_token) {
+		return <Redirect href="/login" />;
+	}
+
 	return (
 		<Stack>
 			<Stack.Screen name="index" />
--- app/(app)/index.tsx
+++ app/(app)/index.tsx
@@ -1,20 +1,6 @@
-import { useAtomValue } from 'jotai';
 import { View, Text } from 'react-native';
-import { authAtom } from '../../entities/auth/model/auth.state';
-import { useEffect } from 'react';
-import { router, useRootNavigationState } from 'expo-router';
 
 export default function MyCourses() {
-	const { access_token } = useAtomValue(authAtom);
-	const state = useRootNavigationState();
-
-	useEffect(() => {
-		if (!state?.key) return;
-		if (!access_token) {
-			router.replace('/login');
-		}
-	}, [access_token, state]);
-
 	return (
 		<View>
 			<Text>index</Text>
