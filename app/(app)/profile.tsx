6e5780be7705d7da67ca8a27f79e25a6ca6276cc
AlariCode <antonlarichev@gmail.com>
2024-02-01 16:50:47 +0300

    Сохранение профиля

--- app/(app)/profile.tsx
+++ app/(app)/profile.tsx
@@ -1,16 +1,36 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { View, StyleSheet } from 'react-native';
 import { ImageUploader } from '../../shared/ImageUploader/ImageUploader';
 import { Gaps } from '../../shared/tokens';
 import { Avatar } from '../../entities/user/ui/Avatar/Avatar';
+import { useAtom } from 'jotai';
+import { updateProfileAtom } from '../../entities/user/model/user.state';
+import { Button } from '../../shared/Button/Button';
 
 export default function Profile() {
 	const [image, setImage] = useState<string | null>(null);
+	const [profile, updateProfile] = useAtom(updateProfileAtom);
+
+	const submitProfile = () => {
+		if (!image) {
+			return;
+		}
+		updateProfile({ photo: image });
+	};
+
+	useEffect(() => {
+		if (profile && profile.profile?.photo) {
+			setImage(profile.profile?.photo);
+		}
+	}, [profile]);
 
 	return (
-		<View style={styles.container}>
-			<Avatar image={image} />
-			<ImageUploader onUpload={setImage} onError={(e) => console.log(e)} />
+		<View>
+			<View style={styles.container}>
+				<Avatar image={image} />
+				<ImageUploader onUpload={setImage} onError={(e) => console.log(e)} />
+			</View>
+			<Button text="Сохранить" onPress={submitProfile} />
 		</View>
 	);
 }
--- entities/user/model/user.state.ts
+++ entities/user/model/user.state.ts
@@ -10,6 +10,41 @@ export const profileAtom = atom<UserState>({
 	error: null,
 });
 
+export const updateProfileAtom = atom(
+	async (get) => {
+		return get(profileAtom);
+	},
+	async (get, set, { photo }: { photo: string }) => {
+		try {
+			const { access_token } = await get(authAtom);
+			const { data } = await axios.patch<User>(
+				API.profile,
+				{
+					photo,
+				},
+				{
+					headers: {
+						Authorization: `Bearer ${access_token}`,
+					},
+				},
+			);
+			set(profileAtom, {
+				isLoading: false,
+				profile: data,
+				error: null,
+			});
+		} catch (error) {
+			if (error instanceof AxiosError) {
+				set(profileAtom, {
+					isLoading: false,
+					profile: null,
+					error: error.response?.data.message,
+				});
+			}
+		}
+	},
+);
+
 export const loadProfileAtom = atom(
 	async (get) => {
 		return get(profileAtom);
