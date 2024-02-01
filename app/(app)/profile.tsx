b30ec561ed5f2cd2921ca93a0899d9b0b9efc8a4
AlariCode <antonlarichev@gmail.com>
2024-02-01 17:12:25 +0300

    SharingAPI

--- app/(app)/profile.tsx
+++ app/(app)/profile.tsx
@@ -6,10 +6,21 @@ import { Avatar } from '../../entities/user/ui/Avatar/Avatar';
 import { useAtom } from 'jotai';
 import { updateProfileAtom } from '../../entities/user/model/user.state';
 import { Button } from '../../shared/Button/Button';
+import * as Sharing from 'expo-sharing';
 
 export default function Profile() {
 	const [image, setImage] = useState<string | null>(null);
 	const [profile, updateProfile] = useAtom(updateProfileAtom);
+
+	const shareProfile = async () => {
+		const isShaingAvailable = await Sharing.isAvailableAsync();
+		if (!isShaingAvailable) {
+			return;
+		}
+		await Sharing.shareAsync('https://purpleschool.ru', {
+			dialogTitle: 'Поделиться профилем',
+		});
+	};
 
 	const submitProfile = () => {
 		if (!image) {
@@ -31,6 +42,7 @@ export default function Profile() {
 				<ImageUploader onUpload={setImage} onError={(e) => console.log(e)} />
 			</View>
 			<Button text="Сохранить" onPress={submitProfile} />
+			<Button text="Поделиться" onPress={shareProfile} />
 		</View>
 	);
 }
