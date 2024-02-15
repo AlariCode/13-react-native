9d835bfd71d1d23c9a8cde81639255f074353f68
AlariCode <antonlarichev@gmail.com>
2024-02-15 18:19:04 +0300

    Переменные окружения

--- shared/api.ts
+++ shared/api.ts
@@ -1,4 +1,4 @@
-export const PREFIX = 'https://purpleschool.ru/api-v2';
+export const PREFIX = `${process.env.EXPO_PUBLIC_DOMAIN}/api-v2`;
 
 export const FILE_API = {
 	uploadImage: `${PREFIX}/files/upload-image?folder=demo`,
